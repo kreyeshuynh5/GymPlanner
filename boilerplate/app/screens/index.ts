53909aef1cbe05a44b68551afb7948013d5aa190
kreyeshuynh <kreyeshuynh@gmail.com>
2024-04-20 12:17:18 -1000

    BoilerPlate commit

--- boilerplate/app/screens/index.ts
+++ boilerplate/app/screens/index.ts
@@ -2,6 +2,7 @@ export * from "./WelcomeScreen"
 // @demo remove-block-start
 export * from "./LoginScreen"
 export * from "./DemoCommunityScreen"
+export * from "./"
 export * from "./DemoDebugScreen"
 export * from "./DemoShowroomScreen/DemoShowroomScreen"
 // @demo remove-block-end
